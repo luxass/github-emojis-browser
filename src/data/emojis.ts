65df32c0ac6ccc3b1d3163f679722586d77d7546
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-07-27 14:18:49 +0200

    feat: include is unicode in emojis

--- src/data/emojis.ts
+++ src/data/emojis.ts
@@ -1,7 +1,11 @@
-import { EMOJI_KEYS, get, getUrl } from "github-emojis";
+import { EMOJI_KEYS, get, getUrl, isUnicodeUrl } from "github-emojis";
 
-export const emojis = EMOJI_KEYS.map((key) => ({
-  emojiKey: key,
-  emoji: get(key),
-  emojiUrl: getUrl(key),
-}));
+export const emojis = EMOJI_KEYS.map((key) => {
+  const emojiUrl = getUrl(key);
+  return {
+    emojiKey: key,
+    emoji: get(key),
+    emojiUrl,
+    isUnicode: isUnicodeUrl(emojiUrl),
+  };
+});
